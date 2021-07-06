9468c82963bac01df8c65de91872f12101640a78
raphaelsoul <raphaelsoul@163.com>
2021-07-06 14:31:22 +0800

    feat: blur frame

--- video/src/main.rs
+++ video/src/main.rs
@@ -6,7 +6,9 @@ use opencv::{highgui, core};
 use opencv::imgcodecs::imread;
 use opencv::prelude::MatTrait;
 use opencv::highgui::{imshow, wait_key, destroy_all_windows};
-use opencv::core::MatTraitManual;
+use opencv::core::{MatTraitManual, Point, BORDER_DEFAULT, BORDER_ISOLATED, Size, BORDER_CONSTANT};
+use opencv::imgproc::{blur, hough_lines_p, canny, erode};
+use opencv::core::BorderTypes::BORDER_REFLECT;
 
 fn main() {
     let img = "test.jpg";
@@ -17,7 +19,7 @@ fn main() {
         println!("img read error");
     } else {
         imshow("show img", &mat).unwrap();
-        wait_key(1000).unwrap();
+        wait_key(100).unwrap();
 
         let mut cam = VideoCapture::new(0, CAP_ANY).unwrap();
         let opened = cam.is_opened().unwrap();
@@ -31,9 +33,13 @@ fn main() {
             let mut frame = core::Mat::default();
             cam.read(&mut frame).unwrap();
 
-            let  size = frame.size().unwrap();
+            let size = frame.size().unwrap();
             if size.width > 0 {
-                highgui::imshow("show img", &mut frame).unwrap();
+                let mut handled = core::Mat::default();
+                // canny(&mut frame, &mut handled, 127.0, 255.0, 100, true);
+                blur(&frame, &mut handled, Size::new(50, 50), Point::new(0, 0), BORDER_DEFAULT);
+
+                highgui::imshow("show img", &mut handled).unwrap();
             }
 
             let key = wait_key(10).unwrap();
